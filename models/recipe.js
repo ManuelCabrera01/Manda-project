3dc7bcb776e5efd57d87f067b858e894cfc58e29
Manuel <m.a.c.p.90@gmail.com>
2017-10-10 18:46:49 -0400

    'recipe created'

--- models/recipe.js
+++ models/recipe.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
-const TYPES    = require('./recipe-types');
+// const TYPES    = require('./recipe-types');
 
 const RecipeSchema = new Schema({
   recipe        : { type: String, required: true },
-  description   : { type: String, required: true },
   ingredients   : { type: String, required: true },
   instructions  : { type: String, required: true },
   _creator      : { type: Schema.Types.ObjectId, ref: 'User', required: true },
--- routes/recipe.js
+++ routes/recipe.js
@@ -3,10 +3,28 @@ const Recipe = require('../models/recipe');
 const router   = express.Router();
 const { ensureLoggedIn }  = require('connect-ensure-login');
 
-
+// reden view that display the form that create new recipe
 router.get('/new', (req, res) => {
  res.render('recipe/new');
 });
 
+//  saving new recipe
+router.post('/', ensureLoggedIn('/login'), (req, res, next) => {
+  const newRecipe = new Recipe({
+     recipe       : req.body.recipe,
+     ingredients  : req.body.ingredients,
+     instructions : req.body.instructions,
+     _creator     : req.user._id,
+     notes        : req.body. notes
+     });
+
+     newRecipe.save( (err) => {
+       if (err) {
+         res.render('recipe/new', { recipe: newRecipe,});
+       } else {
+         res.redirect(`/recipe/${newRecipe._id}`);
+       }
+     });
+  });
 
 module.exports = router;

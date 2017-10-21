0e9cdee380b8ae85668775efad1172a47c0a52a9
Manuel <m.a.c.p.90@gmail.com>
2017-10-21 16:11:17 -0400

    app.js stetic changes

--- app.js
+++ app.js
@@ -21,12 +21,6 @@ mongoose.connect('mongodb://localhost:27017/Manda-development');
 
 const app = express();
 
-
-
-
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
